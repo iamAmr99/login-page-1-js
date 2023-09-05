6883d83661e3b82ffa5556d5d89a9cb6b51c112f
Amr Adel <amradel5599@gmail.com>
2023-09-05 21:01:35 +0300

    v 1.0

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+nav = document.querySelector('nav')
+loginBtn = document.getElementById('loginBtn')
+subimBtn = document.getElementById('submitBtn')
+
+
+
+loginBtn.onclick = function () {
+    nav.style.display = 'block';
+}
+submitBtn.onclick = function () {
+    nav.style.display = 'block';
+}

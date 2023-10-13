e41a9256fdb570d8716a3a450d69f987746e2ac2
iamAmr99 <amradel5599@gmail.com>
2023-10-13 07:41:56 +0300

    added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,31 @@
-nav = document.querySelector('nav')
-loginBtn = document.getElementById('loginBtn')
-submitBtn = document.getElementById('submitBtn')
 
+let loginBtn = document.getElementById('loginBtn')
+let submitBtn = document.getElementById('submitBtn')
 
+let firstName = document.getElementById('firstName')
+let lastName = document.getElementById('lastName')
+let userName = document.getElementById('userName')
+let userPass = document.getElementById('userPass')
 
-loginBtn.onclick = function () {
-    nav.style.display = 'block';
+let users;
+
+if (localStorage.getItem('_db') == null) {
+    users = [];
+} else {
+    JSON.parse(localStorage.getItem('_db'))
 }
-submitBtn.onclick = function () {
-    nav.style.display = 'block';
+
+function submit() {
+    userInputValidation()
+    isExist()
+    if (userInputValidation() == true && isExist() == false) {
+        let user = {
+            firstname: firstName.value,
+            lastname: lastName.value,
+            username: userName.value,
+            userpass: userPass.value
+        }
+        users.push(user)
+        localStorage.setItem('_db', JSON.stringify(users))
+    }
 }
